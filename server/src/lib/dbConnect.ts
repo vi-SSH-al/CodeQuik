969f3e3880360600aa8dfda26ed51d406eaef3e0
vi-SSH-al <vishalmourya229@gmail.com>
2024-09-02 22:27:50 +0530

    Updation Database configuration

--- server/src/lib/dbConnect.ts
+++ server/src/lib/dbConnect.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI!, {
-      dbName: "wd-compiler",
+      dbName: "codequik",
     });
     console.log("connection established!");
   } catch (error) {
